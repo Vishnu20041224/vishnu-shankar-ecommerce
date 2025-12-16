990b7b0922ca7e741b518aa37568452df613901c
Vishnu Shankar <rhvishnushankar@gmil.com>
2025-12-16 12:57:39 +0530

    update env

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json(`Server running on port ${PORT}`);
